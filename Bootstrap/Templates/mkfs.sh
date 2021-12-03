569a98b2f066e6d9129eabfa8e3ef4e08c6a6bc8
Gerulf Schnitzler <gerulf@myprofitsee.com>
2021-12-03 14:59:55 +0100

    add support for partitions

--- Bootstrap/Templates/mkfs.sh
+++ Bootstrap/Templates/mkfs.sh
@@ -1,12 +1,60 @@
 #!/bin/bash
-echo "creating filesystems: "
-mkfs.btrfs -f -L disk1 [% machine.self.RAID.DISK1 %] > /dev/null
-mount  [% machine.self.RAID.DISK1 %] [% paths.hostos.MOUNT %]
+
+#/dev/sda1 	(bootloader) 	2M 		BIOS boot partition
+#/dev/sda2 	fat32 		128M 		Boot/EFI system partition (for future use)
+#/dev/sda3 	btrfs 		Rest 		Root partition 
+# only sda3 need to be formatted for the time being
+# this partition scheme is hardcoded. the 3 for the root partition (the only one cared about so far) is added in various places.
+# the reintroduction of partitions means that support for nvme drives is dropped, as they require an additional 'p' before the '3'.
+# FML.
+
+
+
+echo "partition disk1: "
+# use gpt and Mib
+parted -a optimal [% machine.self.RAID.DISK1 %] mklabel gpt > /dev/null
+parted -a optimal [% machine.self.RAID.DISK1 %] unit mib > /dev/null
+# bios boot partition
+parted -a optimal [% machine.self.RAID.DISK1 %] mkpart primary 1 3  > /dev/null
+parted -a optimal [% machine.self.RAID.DISK1 %] name 1 grub > /dev/null
+parted -a optimal [% machine.self.RAID.DISK1 %] set 1 bios_grub on > /dev/null
+# efi boot partition for later use
+parted -a optimal [% machine.self.RAID.DISK1 %] mkpart primary 3 131 > /dev/null
+parted -a optimal [% machine.self.RAID.DISK1 %] name 2 boot > /dev/null
+parted -a optimal [% machine.self.RAID.DISK1 %] set 2 boot on > /dev/null
+# system
+parted -a optimal [% machine.self.RAID.DISK1 %] mkpart primary 131 100% > /dev/null
+parted -a optimal [% machine.self.RAID.DISK1 %] name 3 rootfs > /dev/null
+
 [% IF machine.self.RAID.LEVEL != 'raidS' %]
 # only do that in raidmodes
-btrfs device add -f [% machine.self.RAID.DISK2 %] [% paths.hostos.MOUNT %]
+echo "partition disk2: "
+# use gpt and Mib
+parted -a optimal [% machine.self.RAID.DISK2 %] mklabel gpt > /dev/null
+parted -a optimal [% machine.self.RAID.DISK2 %] unit mib > /dev/null
+# bios boot partition
+parted -a optimal [% machine.self.RAID.DISK2 %] mkpart primary 1 3  > /dev/null
+parted -a optimal [% machine.self.RAID.DISK2 %] name 1 grub > /dev/null
+parted -a optimal [% machine.self.RAID.DISK2 %] set 1 bios_grub on > /dev/null
+# efi boot partition for later use
+parted -a optimal [% machine.self.RAID.DISK2 %] mkpart primary 3 131 > /dev/null
+parted -a optimal [% machine.self.RAID.DISK2 %] name 2 boot > /dev/null
+parted -a optimal [% machine.self.RAID.DISK2 %] set 2 boot on > /dev/null
+# system
+parted -a optimal [% machine.self.RAID.DISK2 %] mkpart primary 131 100% > /dev/null
+parted -a optimal [% machine.self.RAID.DISK2 %] name 3 rootfs > /dev/null
+[% END %]
+
+echo "creating filesystems: "
+mkfs.btrfs -f -L disk1 [% machine.self.RAID.DISK1 %][% 'p' IF machine.self.RAID.DISK1.match('nvme') %]3 > /dev/null
+mount  [% machine.self.RAID.DISK1 %][% 'p' IF machine.self.RAID.DISK1.match('nvme') %]3 [% paths.hostos.MOUNT %]
+
+[% IF machine.self.RAID.LEVEL != 'raidS' %]
+# only do that in raidmodes
+btrfs device add -f [% machine.self.RAID.DISK2 %][% 'p' IF machine.self.RAID.DISK1.match('nvme') %]3 [% paths.hostos.MOUNT %]
 btrfs balance start -f -dconvert=[% machine.self.RAID.LEVEL %] -mconvert=[% machine.self.RAID.LEVEL %] [% paths.hostos.MOUNT %]
 [% END %]
+
 btrfs subvolume create [% paths.hostos.MOUNT %]/boot
 btrfs subvolume create [% paths.hostos.MOUNT %]/system1
 btrfs subvolume create [% paths.hostos.MOUNT %]/system2
--- Bootstrap/Templates/mount_data.sh
+++ Bootstrap/Templates/mount_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mount -o subvol=data [% machine.self.RAID.DISK1 %] [% paths.data.ROOT %]
+mount -o subvol=data [% machine.self.RAID.DISK1 %][% 'p' IF machine.self.RAID.DISK1.match('nvme') %]3 [% paths.data.ROOT %]
 mkdir -p [% paths.data.IMAGES %]
 mkdir -p [% paths.psi.ROOT %]
